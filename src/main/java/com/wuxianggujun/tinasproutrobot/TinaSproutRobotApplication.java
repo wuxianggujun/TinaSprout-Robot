5df739b48db1f5c0405d211c07132af436e42e80
wuxianggujun <3344207732@qq.com>
2022-12-16 23:28:57 +0800

    先在机器人项目进行mvn clean install，然后使用  maven { url 'file:///C:/Java/maven_repository'}将构建安装的jar导入到我的项目中

--- src/main/java/com/wuxianggujun/tinasproutrobot/TinaSproutRobotApplication.java
+++ src/main/java/com/wuxianggujun/tinasproutrobot/TinaSproutRobotApplication.java
@@ -1,8 +1,10 @@
 package com.wuxianggujun.tinasproutrobot;
 
+import com.zhuangxv.bot.EnableBot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableBot
 @SpringBootApplication
 public class TinaSproutRobotApplication {
 
