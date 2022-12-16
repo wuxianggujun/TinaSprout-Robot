317c05b8b18c8ea81ed60d916041691dc4a93153
wuxianggujun <3344207732@qq.com>
2022-12-16 23:08:01 +0800

    Initial commit

---
+++ src/main/java/com/wuxianggujun/tinasproutrobot/TinaSproutRobotApplication.java
@@ -0,0 +1,13 @@
+package com.wuxianggujun.tinasproutrobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinaSproutRobotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TinaSproutRobotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wuxianggujun/tinasproutrobot/TinaSproutRobotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wuxianggujun.tinasproutrobot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinaSproutRobotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
